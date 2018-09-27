89cde69ebf03b9d22ebf4e3e300083c8ebd1bc87
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-09-28 00:57:47 +0200

    Added basic health check.

---
+++ src/main/java/cloud/nativ/javaee/AlwaysHealthyCheck.java
@@ -0,0 +1,23 @@
+package cloud.nativ.javaee;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Simple health check that always returns UP and healthy.
+ */
+@ApplicationScoped
+@Health
+public class AlwaysHealthyCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.builder()
+                .name("Java EE 8")
+                .withData("message", "Always healthy!")
+                .up()
+                .build();
+    }
+}
