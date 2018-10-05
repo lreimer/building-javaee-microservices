c5cd50858695edf2b27bcbc25225f43fd4fb1621
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-10-05 08:52:00 +0200

    Added Json-P and Json-B demo.

---
+++ src/main/java/cloud/nativ/javaee/advanced/JsonbResource.java
@@ -0,0 +1,127 @@
+package cloud.nativ.javaee.advanced;
+
+import lombok.extern.java.Log;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbConfig;
+import javax.json.bind.annotation.*;
+import javax.json.bind.config.PropertyNamingStrategy;
+import javax.json.bind.config.PropertyOrderStrategy;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.logging.Level;
+
+@Log
+@ApplicationScoped
+@Path("json-b")
+@Produces(MediaType.APPLICATION_JSON)
+public class JsonbResource {
+
+    private Jsonb jsonb;
+
+    private JsonbPojo defaultJsonbPojo;
+    private JsonbPojo customJsonbPojo;
+
+    @PostConstruct
+    @HEAD
+    public void initialize() {
+        defaultJsonbPojo = new JsonbPojo("Hello World. (Default)", 42, LocalDate.now());
+        customJsonbPojo = new JsonbPojo("Hello World. (Custom)", 42, LocalDate.now());
+
+        JsonbConfig jsonbConfig = new JsonbConfig()
+                .withDateFormat("dd.MM.yyyy", Locale.GERMANY)
+                .withFormatting(true)
+                .withPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CASE_WITH_DASHES)
+                .withNullValues(true)
+                .withPropertyOrderStrategy(PropertyOrderStrategy.REVERSE);
+
+        jsonb = JsonbBuilder.newBuilder()
+                .withConfig(jsonbConfig).build();
+    }
+
+    @GET
+    @Path("/default")
+    public JsonbPojo marshall() {
+        return defaultJsonbPojo;
+    }
+
+    @POST
+    @Path("/default")
+    public void unmarshall(JsonbPojo pojo) {
+        LOGGER.log(Level.INFO, "Default Unmarshalled {0}", pojo);
+        this.defaultJsonbPojo = pojo;
+    }
+
+    @GET
+    @Path("/custom")
+    public String marshallCustom() {
+        return jsonb.toJson(customJsonbPojo);
+    }
+
+    @POST
+    @Path("/custom")
+    public void unmarshallCustom(String jsonBody) {
+        customJsonbPojo = jsonb.fromJson(jsonBody, JsonbPojo.class);
+        LOGGER.log(Level.INFO, "Custom Unmarshalled {0}", customJsonbPojo);
+    }
+
+    @JsonbNillable
+    public static class JsonbPojo {
+
+        @JsonbProperty(value = "greeting", nillable = true)
+        private final String message;
+
+        @JsonbNumberFormat("#,##0.00")
+        private final Integer answerToEverything;
+
+        @JsonbDateFormat("yyyy-MM-dd")
+        private final LocalDate today;
+
+        // @JsonbTransient
+        // results in a NullPointerException
+        // annotate getter instead
+        private BigDecimal money = BigDecimal.TEN;
+
+        @JsonbCreator
+        public JsonbPojo(@JsonbProperty(value = "greeting", nillable = true) String message,
+                         @JsonbProperty(value = "answerToEverything", nillable = true) Integer answerToEverything,
+                         @JsonbProperty(value = "today", nillable = true) LocalDate today) {
+            this.message = message;
+            this.answerToEverything = answerToEverything;
+            this.today = today;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public Integer getAnswerToEverything() {
+            return answerToEverything;
+        }
+
+        public LocalDate getToday() {
+            return today;
+        }
+
+        @JsonbTransient
+        public BigDecimal getMoney() {
+            return money;
+        }
+
+        @Override
+        public String toString() {
+            return "JsonbPojo{" +
+                    "message='" + message + '\'' +
+                    ", answerToEverything=" + answerToEverything +
+                    ", today=" + today +
+                    ", money=" + money +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/cloud/nativ/javaee/advanced/JsonpResource.java
@@ -0,0 +1,61 @@
+package cloud.nativ.javaee.advanced;
+
+import lombok.extern.java.Log;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+@Log
+@ApplicationScoped
+@Path("json-p")
+@Produces(MediaType.APPLICATION_JSON)
+public class JsonpResource {
+
+    private JsonArray jsonArray;
+
+    @PostConstruct
+    @HEAD
+    public void initialize() {
+        this.jsonArray = Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                        .add("aString", "Hello Json-P 1")
+                        .add("aInteger", 42)
+                        .add("aBoolean", false)
+                        .add("aNullValue", JsonValue.NULL))
+                .add(Json.createObjectBuilder()
+                        .add("aString", "Hello Json-P 2")
+                        .add("aInteger", 23)
+                        .add("aBoolean", true))
+                .build();
+    }
+
+    @GET
+    public JsonArray marshall() {
+        return jsonArray;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void unmarshall(InputStream jsonBody) {
+        try (JsonReader reader = Json.createReader(jsonBody)) {
+            this.jsonArray = reader.readArray();
+        }
+
+        LOGGER.log(Level.INFO, "Unmarshalled JSON-P {0}.", jsonArray);
+    }
+
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
+    public void patch(JsonArray jsonPatchArray) {
+        LOGGER.log(Level.INFO, "Unmarshalled JSON-P Patch {0}.", jsonPatchArray);
+
+        JsonPatch jsonPatch = Json.createPatchBuilder(jsonPatchArray).build();
+        this.jsonArray = jsonPatch.apply(jsonArray);
+        LOGGER.log(Level.INFO, "Patched {0}.", jsonArray);
+    }
+}
